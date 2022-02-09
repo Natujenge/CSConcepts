417cc2b81e7592db8b6d5556666916f2857a8f39
Kamochu <kamochu@gmail.com>
2022-02-09 04:01:59 +0300

    initialized the maven project added TestServer and TestClient to demonstrate the sending of numbers over network

---
+++ src/main/java/natujenge/csconcepts/App.java
@@ -0,0 +1,13 @@
+package natujenge.csconcepts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
